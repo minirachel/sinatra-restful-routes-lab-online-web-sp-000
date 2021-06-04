939e5239153e8d733999afc1e4bbf07b45d8377a
minirachel <rachel.a.levinson@gmail.com>
2021-06-04 16:37:32 -0400

    commit tree a79b42e9fdf164554ee8a30fe85a818d4e50d271

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,43 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+    #need to put this before :id because it would try to match new to an id: pair
+    get "/recipes/new" do
+      erb :new
+    end
+
+  get "/recipes" do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  post "/recipes" do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+
+  get "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  delete "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(params[:recipe])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
 
 end
